e27466107536e61786064193b95dff0347a10444
Lisa Jacklin <lmjacklin@alaska.edu>
2024-08-14 11:57:55 -0800

    Main File linked to GUI

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-%main python script
+#Lisa Jacklin
+#WordWeaver
+#2024-08-14
+
+from tkinter import Tk
+from gui import MainApp
+
+if __name__ == "__main__":
+	root=Tk()
+	app = MainApp(root)
+	root.mainloop()
+
+
+
 
