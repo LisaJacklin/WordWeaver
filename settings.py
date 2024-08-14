dfae91015679186416b1e35b1a3bc36c1e224fe2
Lisa Jacklin <lmjacklin@alaska.edu>
2024-08-14 14:03:52 -0800

    outline settings included

---
+++ settings.py
@@ -0,0 +1,10 @@
+#Lisa Jacklin
+#WordWeaver
+#2024-08-14
+
+#basic outline settings line saving defaults, etc...
+
+SAVE_DIRECTORY= "saves/"
+DEFAULT_BACKGROUND = "assets/default_background.png"
+
+
